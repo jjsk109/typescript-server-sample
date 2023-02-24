28e24833bfd9d3875ee64c52860f8455d4b5e99f
장익순(NickSoon) <39641848+jjsk109@users.noreply.github.com>
2023-02-24 18:42:11 +0900

    readme 작성

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,19 @@ import morgan from 'morgan';
 const app = express();
 const port = 3012;
 
+// import cors from 'cors';
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+// }));
+
+
 app.use(
   helmet({
     contentSecurityPolicy: {
